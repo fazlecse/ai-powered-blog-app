b2cf1a0d67f76db0f4e12eae10f44b101f110182
Fazle Rabbi <fazlerabbi@Fazles-MacBook-Air.local>
2025-11-05 20:58:04 +0600

    admin-> dashboard

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import Home from "../pages/Home";
 import { createBrowserRouter } from "react-router";
 import Blog from "../pages/Blog";
+import AdminLayout from "../pages/admin/AdminLayout";
+import Dashboard from "../pages/admin/Dashboard";
+import AddBlog from "../pages/admin/AddBlog";
+import ListBlog from "../pages/admin/ListBlog";
+import Comments from "../pages/admin/Comments";
+import Login from "../components/admin/Login";
 
 const router = createBrowserRouter([
   {
@@ -10,7 +16,29 @@ const router = createBrowserRouter([
   },
   {
     path: "/blog/:id",
-    element: <Blog />, 
+    element: <Blog />,
+  },
+  {
+    path: "/admin",
+    element: true ? <AdminLayout /> : <Login />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+      },
+      {
+        path: "addBlog",
+        element: <AddBlog />,
+      },
+      {
+        path: "listBlog",
+        element: <ListBlog />,
+      },
+      {
+        path: "comments",
+        element: <Comments />,
+      },
+    ],
   },
 ]);
 
